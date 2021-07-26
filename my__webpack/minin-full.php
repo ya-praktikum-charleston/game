c9c199bcf24ccb6117275f0683d26350ceaa9cf9
vit-vokhminov <46236101+vit-vokhminov@users.noreply.github.com>
2021-07-26 23:21:13 +0700

    webpack

--- my__webpack/minin-full.php
+++ my__webpack/minin-full.php
@@ -479,7 +479,297 @@
 
 </div>
 
+<div class="linear" id="use_strict">
 
+    <h2>Готовая настройка package.json</h2>
+
+    <pre class="brush: js;">
+        {
+          "name": "my__webpack",
+          "version": "1.0.0",
+          "description": "webpack road map",
+          "private": true,
+          "scripts": {
+            "dev": "cross-env NODE_ENV=development webpack --mode development",
+            "build": "cross-env NODE_ENV=production webpack --mode production",
+            "watch": "cross-env NODE_ENV=development webpack --mode development --watch",
+            "start": "cross-env NODE_ENV=development webpack serve --mode development --open",
+            "stats": "webpack --json > stats.json && webpack-bundle-analyzer stats.json"
+          },
+          "keywords": [
+            "js",
+            "webpack"
+          ],
+          "author": "vit-vokhminov",
+          "license": "ISC",
+          "dependencies": {
+            "@babel/polyfill": "^7.12.1",
+            "jquery": "^3.6.0",
+            "normalize.css": "^8.0.1"
+          },
+          "browserslist": "> 0.25%, not dead",
+          "devDependencies": {
+            "@babel/core": "^7.14.8",
+            "@babel/plugin-proposal-class-properties": "^7.14.5",
+            "@babel/preset-env": "^7.14.8",
+            "@babel/preset-typescript": "^7.14.5",
+            "assets-webpack-plugin": "^7.1.1",
+            "babel-eslint": "^10.1.0",
+            "babel-loader": "^8.2.2",
+            "clean-webpack-plugin": "^4.0.0-alpha.0",
+            "copy-webpack-plugin": "^9.0.1",
+            "cross-env": "^7.0.3",
+            "css-loader": "^6.2.0",
+            "csv-loader": "^3.0.3",
+            "eslint": "^7.31.0",
+            "eslint-config-google": "^0.14.0",
+            "eslint-loader": "^4.0.2",
+            "file-loader": "^6.2.0",
+            "html-webpack-plugin": "^5.3.2",
+            "less": "^4.1.1",
+            "less-loader": "^10.0.1",
+            "mini-css-extract-plugin": "^2.1.0",
+            "node-sass": "^6.0.1",
+            "optimize-css-assets-webpack-plugin": "^6.0.1",
+            "sass-loader": "^12.1.0",
+            "style-loader": "^3.2.1",
+            "terser-webpack-plugin": "^5.1.4",
+            "webpack": "^5.46.0",
+            "webpack-bundle-analyzer": "^4.4.2",
+            "webpack-cli": "^4.7.2",
+            "webpack-dev-server": "^3.11.2",
+            "xml-loader": "^1.2.1"
+          }
+        }
+    </pre>
+
+</div>
+
+<div class="linear" id="use_strict">
+
+    <h2>Готовая настройка webpack.config.js</h2>
+
+    <pre class="brush: js;">
+        // подключаем пути
+        const path = require('path');
+        const HTMLWebpackPlugin = require('html-webpack-plugin');
+        const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+        const CopyWebpackPlugin = require('copy-webpack-plugin');
+        const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+        const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+        const TerserWebpackPlugin = require('terser-webpack-plugin');
+        const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+        const isDev = process.env.NODE_ENV === 'development';
+        const isProd = !isDev;
+
+        const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+        const optimization = () => {
+            const config = {
+                splitChunks: {
+                    chunks: 'all'
+                }
+            }
+
+            if (isProd) {
+                config.minimizer = [
+                    new OptimizeCssAssetsPlugin(),
+                    new TerserWebpackPlugin()
+                ]
+            }
+
+            return config
+        }
+
+        const cssLoaders = extra => {
+            const loaders = [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // hmr: isDev,         // перезагрузка css файлов во время изменения
+                        // reloadAll: true
+                    },
+                },
+                'css-loader'
+            ]
+            // если есть less, sass или что то такое, то добавить в массив
+            if (extra) {
+                loaders.push(extra)
+            }
+
+            return loaders
+        }
+        const babelOptions = preset => {
+            const options = {
+                presets: [
+                    '@babel/preset-env'
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties'
+                ]
+            }
+
+            if (preset) {
+                options.presets.push(preset);
+            }
+
+            return options;
+        }
+        const jsLoaders = () => {
+            const loaders = [{
+                loader: 'babel-loader',
+                options: babelOptions()
+            }]
+
+            if (isDev) {
+                loaders.push('eslint-loader');
+            }
+
+            return loaders
+        }
+        const plugins = () => {
+            const base = [
+                // Позволяет взимодействовать с html
+                new HTMLWebpackPlugin({
+                    // пути прописываются исходя из context
+                    template: './index.html',
+                    minify: {
+                        collapseWhitespace: isProd  // минифицировать файл html в продакшене
+                    }
+                }),
+                // очищает папку dist
+                new CleanWebpackPlugin(),
+                // копировать файлы на примере favicon
+                new CopyWebpackPlugin({
+                    patterns: [
+                        {
+                            from: path.resolve(__dirname, 'src/favicon.ico'),
+                            to: path.resolve(__dirname, 'dist')
+                        }
+                    ]
+                }),
+                new MiniCssExtractPlugin({
+                    filename: filename('css'),
+                })
+            ]
+
+            if (isProd) {
+                base.push(new BundleAnalyzerPlugin())
+            }
+
+            return base
+        }
+
+        module.exports = {
+            // говорит где лежат все исходники приложения
+            context: path.resolve(__dirname, 'src'),
+            // как webpack будет собирать проект, в каком режиме (разработка или продакшен)
+            mode: "development",
+            // Входной файл webPack, файлы подключаются в filename
+            entry: {
+                // пути прописываются исходя из context
+                main: ['@babel/polyfill', './index.js'],
+                analytics: './analytics.ts'
+            },
+            // куда сдкладывать результат работы webPack
+            output: {
+                // файл запуска
+                // [name] это паттерно, в который передаются ключи из entry
+                // [hash] это отсутствия кэша
+                // filename() это оптимизация, т.к. filename есть в нескольких местах
+                filename: filename('js'),
+                // папка с копиляцией
+                path: path.resolve(__dirname, 'dist')
+            },
+            resolve: {
+                // говорим webpack какие расширения нужно понимать по умолчанию и не указывать их при import файлов
+                extensions: ['.js', '.json', '.png'],
+                // переменные для указания путей
+                alias: {
+                    '@assets': path.resolve(__dirname, 'src/assets'),
+                    '@': path.resolve(__dirname, 'src'),
+                }
+            },
+            optimization: optimization(),
+            // автообновление
+            devServer: {
+                port: 4200,
+                hot: isDev      // обновляет только то что изменилось, мгновенное реагирование
+            },
+            plugins: plugins(),
+            // подключение loaders
+            module: {
+                // rules - правила
+                rules: [
+                    {
+                        // если webpack попадаюется css используй loaders
+                        // для MiniCssExtractPlugin нужно подключить плагин new MiniCssExtractPlugin
+                        test: /\.css$/,
+                        use: cssLoaders()
+                    },
+                    // распознавание less
+                    {
+                        test: /\.less$/,
+                        use: cssLoaders('less-loader')
+                    },
+                    {
+                        test: /\.s[ac]ss$/,
+                        use: cssLoaders('sass-loader')
+                    },
+                    {
+                        // распознавание картинок
+                        test: /\.(png|jpg|jpeg|svg|gif)$/,
+                        dependency: { not: ['url'] },
+                        use: [
+                            {
+                                loader: 'file-loader',
+                                options: {
+                                    esModule: false
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        // распознавание шрифтов
+                        test: /\.(ttf|woff|woff2|eot)$/,
+                        dependency: { not: ['url'] },
+                        use: [
+                            {
+                                loader: 'file-loader',
+                                options: {
+                                    esModule: false
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        // распознавание xml файлов
+                        test: /\.xml$/,
+                        use: ['xml-loader']
+                    },
+                    {
+                        // распознавание csv файлов
+                        test: /\.csv$/,
+                        use: ['csv-loader']
+                    },
+                    {
+                        test: /\.js$/,
+                        exclude: /(node_modules|bower_components)/,        // не учитывать исходники библиотек
+                        use: jsLoaders()
+                    },
+                    {
+                        test: /\.ts$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
+                        options: babelOptions('@babel/preset-typescript')
+                    },
+                ]
+            }
+        }
+    </pre>
+
+</div>
 <!--
 
     <div class="linear" id="use_strict">
