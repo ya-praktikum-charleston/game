7135a3d8d2815d8146fc3edc0e0c3db0e824a3b1
Gavrilkov Alexandr <alexgavr89@gmail.com>
2021-08-05 21:42:15 +0500

    first commit

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["stylelint-config-standard",
+            "stylelint-config-rational-order"],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+    // Дополнительные правила
+  }
+};
