1c2062fd5ab69f6c07fad4a2e74e002df7b21c4f
Gavrilkov Alexandr <alexgavr89@gmail.com>
2021-08-26 21:05:18 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Start from './pages/start';
-import ErrorPage from './pages/error';
-import LeaderboardPage from './pages/leaderboard';
-import ForumPage from './pages/forum';
-import Profile from './pages/profile';
-import Signin from './pages/signin';
-import Signup from './pages/signup';
-import Background from './assets/img/Background2.png';
-import ErrorBoundary from './utilities/ErrorBoundary';
-import PrivateRoute from './components/routes/privete';
-
-import './assets/style.css';
-
-export default function App() {
-    return (
-        <div className="app">
-            <div className="canvas" style={{ backgroundImage: `url(${Background})` }}> </div>
-            <ErrorBoundary>
-                <Switch>
-                    <PrivateRoute path="/" exact><Start /></PrivateRoute>
-                    <Route path="/signin"><Signin /></Route>
-                    <Route path="/signup"><Signup /></Route>
-                    <PrivateRoute path="/profile"><Profile /></PrivateRoute>
-                    <PrivateRoute path="/forum"><ForumPage /></PrivateRoute>
-                    <PrivateRoute path="/leaderboard"><LeaderboardPage /></PrivateRoute>
-                    <Route path="/500"><ErrorPage number={500} /></Route>
-                    <Route path="*"><ErrorPage number={404} /></Route>
-                </Switch>
-            </ErrorBoundary>
-        </div>
-    );
-=======
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Start } from "./pages";
-import ErrorPage from "./pages/error/error";
+import ErrorPage from "./pages/error";
 import LeaderboardPage from "./pages/leaderboard";
 import ForumPage from "./pages/forum";
 import Profile from "./pages/profile";
@@ -69,5 +34,4 @@ export default function App() {
 			</ErrorBoundary>
 		</div>
 	);
->>>>>>> 88894c65af3622dc9a33af78c271deb116542219
 }
