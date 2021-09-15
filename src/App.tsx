27cb8c3c330d35f90247f40e4d2718266a2eaa75
Jhon Smir <sir.jsmir@yandex.ru>
2021-09-15 21:49:58 +0500

    ssr add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import routes from './routes';
 
 export default function App() {
     return (
         <>
+
             <div>asdasd</div>
-            <Switch>
-                {routes.map(({ ...routeProps }) => (
-                    <Route key={routeProps.path} {...routeProps} />
-                ))}
-            </Switch>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/topics">Topics</Link>
+                    </li>
+                </ul>
+
+                <Switch>
+                    <Route path="/about">
+                        <div>About</div>
+                    </Route>
+                    <Route path="/topics">
+                        <div>Topics</div>
+                    </Route>
+                    <Route path="/">
+                        <div>Home</div>
+                    </Route>
+                </Switch>
+            </div>
+
         </>
     );
 }
