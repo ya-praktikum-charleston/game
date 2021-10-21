077d71ef202fbc7733e3e156a4627da364035dc9
alexgavr89 <55540013+alexgavr89@users.noreply.github.com>
2021-10-21 05:01:14 +0500

    Добавлен Docker. (#25)
    * fix_start
    
    * fix_start
    
    * docker
    
    * fix

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,12 @@
 const {Sequelize} = require('sequelize')
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,        // Название БД
-    process.env.DB_USER,        // Пользователь
-    process.env.DB_PASSWORD,    // ПАРОЛЬ
+    process.env.POSTGRES_DB,        // Название БД
+    process.env.POSTGRES_USER,        // Пользователь
+    process.env.POSTGRES_PASSWORD,    // ПАРОЛЬ
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.POSTGRES_PORT,
     }
 )
--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ const cors = require('cors');
 const router = require('./routes/index');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.EXPRESS_PORT || 5000;
 
 // const store = create(initialState);
 
