fe4cc715470c521078ea8e75ef6385b32441705e
Jhon Smir <sir.jsmir@yandex.ru>
2021-10-30 13:09:05 +0500

    marge_2

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-module.exports = new Sequelize(
+export default new Sequelize(
     process.env.POSTGRES_DB,        // Название БД
     process.env.POSTGRES_USER,        // Пользователь
     process.env.POSTGRES_PASSWORD,    // ПАРОЛЬ
