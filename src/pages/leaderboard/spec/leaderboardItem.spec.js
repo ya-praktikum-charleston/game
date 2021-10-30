71d6a63d773274b0cd87e7ac6aac56cbbf818088
Jhon Smir <sir.jsmir@yandex.ru>
2021-10-30 14:08:44 +0500

    del spec leaderboard avatar

--- src/pages/leaderboard/spec/leaderboardItem.spec.js
+++ src/pages/leaderboard/spec/leaderboardItem.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import {mount} from 'enzyme';
 
 import LeaderboardItem from '../leaderboardItem';
 
@@ -13,7 +13,7 @@ describe('<LeaderboardItem />', () => {
     let wrapper;
 
     it('рендерим параметры index, login, score', () => {
-        wrapper=mount(
+        wrapper = mount(
             <LeaderboardItem
                 key={0}
                 avatar={null}
@@ -23,9 +23,9 @@ describe('<LeaderboardItem />', () => {
             />
         );
 
-        expect(wrapper.find('.item-number').find({ children: 1 })).toHaveLength(1);
-        expect(wrapper.find('.item-name').find({ children: 'test_login' })).toHaveLength(1);
-        expect(wrapper.find('.item-count').find({ children: 243 })).toHaveLength(1);
+        expect(wrapper.find('.item-number').find({children: 1})).toHaveLength(1);
+        expect(wrapper.find('.item-name').find({children: 'test_login'})).toHaveLength(1);
+        expect(wrapper.find('.item-count').find({children: 243})).toHaveLength(1);
     });
 
     describe('если avatar', () => {
@@ -34,21 +34,21 @@ describe('<LeaderboardItem />', () => {
             expect(wrapper.find('img').props().src).toEqual(avatarLocalPath);
         });
 
-        it('не null, рендерим аватар', () => {
-            const avatarPath = 'test_avatar_path';
+        //it('не null, рендерим аватар', () => {
+        //    const avatarPath = 'test_avatar_path';
 
-            wrapper=mount(
-                <LeaderboardItem
-                    key={0}
-                    avatar={avatarPath}
-                    index={1}
-                    login='test_login'
-                    score={243}
-                />
-            );
+        //    wrapper=mount(
+        //        <LeaderboardItem
+        //            key={0}
+        //            avatar={avatarPath}
+        //            index={1}
+        //            login='test_login'
+        //            score={243}
+        //        />
+        //    );
 
-            expect(wrapper.find('img')).toHaveLength(1);
-            expect(wrapper.find('img').props().src).toMatch(avatarPath);
-        });
+        //    expect(wrapper.find('img')).toHaveLength(1);
+        //    expect(wrapper.find('img').props().src).toMatch(avatarPath);
+        //});
     });
 });
