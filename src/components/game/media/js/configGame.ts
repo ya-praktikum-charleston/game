534a542c8a9133da5265bbc7807f67a509ddfc8a
Jhon Smir <sir.jsmir@yandex.ru>
2021-10-31 16:11:04 +0500

    fix speed gravity

--- src/components/game/media/js/configGame.ts
+++ src/components/game/media/js/configGame.ts
@@ -1,15 +1,22 @@
 export const pixelDevice: number = (window.devicePixelRatio > 1) ? 2 : 1;
 // стартовая скорость игры
 let startSpeed = 0;
+let gravity = 0;
+let step = 0;
 if (pixelDevice <= 1) {
-    startSpeed = 5;
+    startSpeed = 6;
+    gravity = 0.6;
+    step = 0.01;
 } else {
     startSpeed = 3;
+    gravity = 0.4;
+    step = 0.005;
 }
 export default {
     defaultSpeed: startSpeed,
     speedGame: startSpeed,
-    gravity: 0.4,
+    gravity: gravity,
+    step: step,
     isPause: true,
     isGameStopped: false,
     isGameStatic: true,
