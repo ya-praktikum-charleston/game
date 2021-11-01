b268336f434ec161f810c0b1d5692e10d2692ecd
Jhon Smir <sir.jsmir@yandex.ru>
2021-11-01 09:37:43 +0500

    fix pixelDevice

--- src/components/game/media/js/configGame.ts
+++ src/components/game/media/js/configGame.ts
@@ -1,14 +1,16 @@
-export const pixelDevice: number = (window.devicePixelRatio > 1) ? 2 : 1;
+export const pixelDevice: number = window.devicePixelRatio;
 // стартовая скорость игры
 let startSpeed = 0;
 let gravity = 0;
 let step = 0;
 if (pixelDevice <= 1) {
-    startSpeed = 6;
-    gravity = 0.6;
+    console.log(pixelDevice, '123')
+    startSpeed = 5;
+    gravity = 0.5;
     step = 0.01;
 } else {
-    startSpeed = 3;
+    console.log(pixelDevice, 'hjhf')
+    startSpeed = 4;
     gravity = 0.4;
     step = 0.005;
 }
